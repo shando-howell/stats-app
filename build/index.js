c730719b44b0708457a5aec4936e640544d9355f
shandohowell <shandohowell94@gmail.com>
2024-12-24 20:25:05 -0500

    Added CSV file reader logic.

--- build/index.js
+++ build/index.js
@@ -1,14 +1,23 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default.readFileSync('football.csv', {
-    encoding: 'utf-8'
-})
-    .split('\n')
-    .map((row) => {
-    return row.split(',');
-});
-console.log(matches);
+const CSVFileReader_1 = require("./CSVFileReader");
+const reader = new CSVFileReader_1.CSVFileReader('football.csv');
+reader.read();
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+;
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Manchester United won ${manUnitedWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-import fs from 'fs';
+import { CSVFileReader } from "./CSVFileReader";
 
-const matches = fs.readFileSync('football.csv', {
-    encoding: 'utf-8'
-})
-.split('\n')
-.map((row: string): string[] => {
-    return row.split(',');
-});
+const reader = new CSVFileReader('football.csv');
+reader.read();
 
-console.log(matches);
+// enum - enumeration
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+};
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+
+console.log(`Manchester United won ${manUnitedWins} games`);
